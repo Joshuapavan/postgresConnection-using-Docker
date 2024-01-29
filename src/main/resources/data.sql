ed2932209a575c8ac32574b7d955ddc5b2586e07
Joshua Pavan <joshuapavan35@gmail.com>
2024-01-29 12:26:56 +0530

    adds code to execute sql files when the main class is executed.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO widgets VALUES
+(1, 'Widget 1', 'Widget 1 for test'),
+(2, 'Widget 2', 'Widget 2 for test'),
+(3, 'Widget 3', 'Widget 3 for test'),
+(4, 'Widget 4', 'Widget 4 for test'),
+(5, 'Widget 5', 'Widget 5 for test')
+;
